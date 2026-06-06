3f4e310ac3ec58ddd8ad6ccc2dfe6d285ecbc1f6
David Kurek <david.kurek@hotmail.de>
2026-06-06 23:24:56 +0200

    chore: --muted is now default placehoder color for input and textarea

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
 			type={type}
 			data-slot='input'
 			className={cn(
-				'h-9 w-full min-w-0 rounded-md border border-input bg-input px-3 py-1 text-body-sm text-foreground transition-colors outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-button file:font-medium file:text-foreground placeholder:text-muted focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
+				'h-9 w-full min-w-0 rounded-md border border-input bg-input px-3 py-1 text-body-sm text-foreground transition-colors outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-button file:font-medium file:text-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
 				className
 			)}
 			{...props}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,18 +1,18 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
-  return (
-    <textarea
-      data-slot="textarea"
-      className={cn(
-        "flex field-sizing-content min-h-16 w-full resize-none rounded-md border border-input bg-input px-3 py-3 text-body-sm text-foreground transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30",
-        className
-      )}
-      {...props}
-    />
-  )
+function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
+	return (
+		<textarea
+			data-slot='textarea'
+			className={cn(
+				'flex field-sizing-content min-h-16 w-full resize-none rounded-md border border-input bg-input px-3 py-3 text-body-sm text-foreground transition-colors outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
+				className
+			)}
+			{...props}
+		/>
+	);
 }
 
-export { Textarea }
+export { Textarea };
