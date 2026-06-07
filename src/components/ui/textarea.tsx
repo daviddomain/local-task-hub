1b39e5c88102cde2e1d754ac4db091e528d9c12c
David Kurek <david.kurek@hotmail.de>
2026-06-07 11:57:37 +0200

    chore: removed text-body-sm

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
 		<textarea
 			data-slot='textarea'
 			className={cn(
-				'flex field-sizing-content min-h-16 w-full resize-none text-sm rounded-md border bg-input px-3 py-3 text-body-sm text-foreground transition-colors outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
+				'flex field-sizing-content min-h-16 w-full resize-none text-sm rounded-md border bg-input px-3 py-3 text-foreground transition-colors outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
 				className
 			)}
 			{...props}
